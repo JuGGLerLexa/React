ce37fad85f49421116d250d46f9eca2ac4beb675
Олексій <71986453+JuGGLerLexa@users.noreply.github.com>
2022-06-27 15:58:24 +0300

    fixk

--- lesson1/task3/webpack.config.js
+++ lesson1/task3/webpack.config.js
@@ -1,22 +1,26 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
 
 module.exports = {
+
   module: {
     rules: [
       {
         test: /.js$/,
-        use: ["babel-loader"]
-
+        use: ["babel-loader"],
       },
       {
         test: /.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    })
-  ]
-}
+      template: "./src/index.html",
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'review_build')
+  },
+};
