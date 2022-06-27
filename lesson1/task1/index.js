cf3e967f7fa06b9acfcdfe5ead754d12f2d12001
Олексій <71986453+JuGGLerLexa@users.noreply.github.com>
2022-06-27 23:18:25 +0300

    refact

--- lesson1/task1/index.js
+++ lesson1/task1/index.js
@@ -1,17 +1,7 @@
-/* eslint-disable no-undef */
-
-// put your code here
-
 const rootElement = document.querySelector('#root');
 
-// const greetingElem = document.createElement('div');
-// greetingElem.classList.add('greeting');
-// greetingElem.textContent = 'Hello, React!';
-
-// rootElement.append(wrapperElem);
-
 const greetingElem = React.createElement(
-  'div', 
+  'div',
   { className: 'greeting' },
   'Hello, React!'
 );
