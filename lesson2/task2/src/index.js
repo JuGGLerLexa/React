5348874e26f0c0581ca1fd243b64900d8d5dcce2
Олексій <71986453+JuGGLerLexa@users.noreply.github.com>
2022-06-28 15:12:36 +0300

    JSX - attributes

--- lesson2/task2/src/index.js
+++ lesson2/task2/src/index.js
@@ -1,16 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./styles.css";
 
 const rootElement = document.querySelector("#root");
-
+const style = { color: 'red', fontWeight: 700 }
 const element = (
-  <>
-  <h1>Search Form</h1>
-  <div>
-    <input type="text" />
-    <button>Submit</button>
-  </div>
-  </>
+  <main className="page">
+    <form className="login-form">
+      <h1 className="form-title">Profile</h1>
+      <div className="form-control">
+        <label className="form-label" htmlFor="name">
+          Name
+        </label>
+        <input
+          className="form-input"
+          type="text"
+          id="name"
+          name="name"
+          value="anonymous"
+        />
+      </div>
+      <div className="form-control">
+        <label className="form-label" id="age" htmlFor="age">
+          Age
+        </label>
+        <input className="form-input" type="number" value="17" name="age" />
+        <span style={style}>To young</span>
+      </div>
+      <button className="submit-button" type="submit">
+        Submit
+      </button>
+    </form>
+  </main>
 );
 
 ReactDOM.render(element, rootElement);
--- lesson2/task2/webpack.config.js
+++ lesson2/task2/webpack.config.js
@@ -1,25 +1,50 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /.js$/,
-        use: ['babel-loader'],
-      },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  const config = {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /.js$/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /.s?css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new webpack.ProgressPlugin(),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
     ],
-  },
-  plugins: [
-    new webpack.ProgressPlugin(),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
+    devServer: {
+      hot: true,
+    },
+  };
+
+  if (isProduction) {
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      }),
+    );
+  }
+
+  return config;
 };
