e89ef41da1bbcaed5dc6933f80ea8539f3c8c5f3
Олексій <71986453+JuGGLerLexa@users.noreply.github.com>
2022-06-28 16:06:38 +0300

    JSX - отрисовка элементов - next steps // clock

---
+++ lesson2/task3/src/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM, { render } from "react-dom";
+import "./styles.css";
+
+const rootElement = document.querySelector("#root");
+
+const renderSeconds = time => {
+  const seconds = new Date(time).getSeconds();
+  const backgroundColor = seconds % 2 === 0
+    ? '#fff'
+    : '#000';
+
+  const textColor = seconds % 2 !== 0
+    ? '#fff'
+    : '#000';
+
+  const styles = {
+    color: textColor,
+    backgroundColor
+  }
+
+  const element = (
+    <div
+      className="seconds"
+      style={styles}
+    >
+      {seconds}
+    </div>
+  );
+
+  ReactDOM.render(element, rootElement);
+}
+
+renderSeconds(new Date());
+
+setInterval(() => renderSeconds(new Date()), 1000);
