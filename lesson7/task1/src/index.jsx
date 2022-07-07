f0533266d5701c9d7c174e0067f8fd7dceeeebfa
Олексій <71986453+JuGGLerLexa@users.noreply.github.com>
2022-07-07 18:53:03 +0300

    Rendering list

--- lesson7/task1/src/index.jsx
+++ lesson7/task1/src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import NumberList from './NumberList';
+import NumberList from './NumbersList';
 
 const rootElement = document.querySelector("#root");
 
