7eee936070554f126b677100cb9c6e9b30c37443
Олексій <71986453+JuGGLerLexa@users.noreply.github.com>
2022-07-11 20:04:38 +0300

    componentDidUpdate text/log

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -18,7 +18,7 @@ class Life extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    console.log('constructor: good place to create state');
+    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
   };
 
   componentWillUnmount() {
