73be8ccda36ff967f0c8327380e1dcbc2b468e0b
Олексій <71986453+JuGGLerLexa@users.noreply.github.com>
2022-07-12 19:03:28 +0300

    destr, refact

--- lesson8/task3/src/Dimensions.jsx
+++ lesson8/task3/src/Dimensions.jsx
@@ -1,40 +1,30 @@
 import React, { Component } from "react";
 
 class Dimensions extends Component {
-  state = {
-    width: null,
+  state = { 
+    width: null, 
     height: null,
-  };
+   };
 
   componentDidMount() {
     window.addEventListener("resize", this.onResize);
-
     const { innerWidth, innerHeight } = window;
     this.setDimensions(innerWidth, innerHeight);
   }
-
   componentWillUnmount() {
-    window.addEventListener("resize", this.onResize);
+    window.removeEventListener("resize", this.onResize);
   }
-
   onResize = (e) => {
     const { innerWidth, innerHeight } = e.target;
     this.setDimensions(innerWidth, innerHeight);
   };
-
   setDimensions = (width, height) => {
-    this.setState({
-      width,
-      height,
-    });
-    document.title = `${innerWidth} x ${innerHeight}`;
+    this.setState({ width, height });
+    document.title = `${width} x ${height}`;
   };
-
   render() {
     return (
-      <div className="dimensions">
-        {`${this.state.width}px - ${this.state.height}px`}
-      </div>
+      <div className="dimensions">{`${this.state.width}px - ${this.state.height}px`}</div>
     );
   }
 }
