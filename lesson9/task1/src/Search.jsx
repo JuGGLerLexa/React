6d573ba5412dc951e136b9b2b3984bc58047bdf4
Олексій <71986453+JuGGLerLexa@users.noreply.github.com>
2022-07-13 14:01:46 +0300

    Controlled component

---
+++ lesson9/task1/src/Search.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class Search extends Component {
+  state = { value: "" };
+
+  handleChange = (event) => {
+    this.setState({ value: event.target.value });
+  };
+
+  search = () => {
+    event.preventDefault();
+    console.log(this.state.value);
+  };
+
+  render() {
+    return (
+      <form className="search" onSubmit={this.search}>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.value}
+          className="search__input"
+        />
+        <button className="search__button" type="submit">
+          Search
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Search;
---
+++ lesson9/task1/src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import Search from "./Search";
+
+const rootElement = document.querySelector("#root");
+
+ReactDOM.render(<Search />, rootElement);
