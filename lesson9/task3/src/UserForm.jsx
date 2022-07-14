a13d68a429908c35de232d7747c2622819b39e37
Олексій <71986453+JuGGLerLexa@users.noreply.github.com>
2022-07-14 17:22:20 +0300

    in progress

--- lesson9/task3/src/UserForm.jsx
+++ lesson9/task3/src/UserForm.jsx
@@ -1,39 +1,27 @@
 import React, { Component } from "react";
 
 class UserForm extends Component {
-  state = {
-    name: "",
-    student: "",
-    occupation: "Sidney",
-    about: "",
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = [...new FormData(this.formRef)].reduce(
+      (acc, [name, value]) => ({ ...acc, [name]: value }),
+      {}
+    );
   };
-  handeleChange = (event) => {
-    const { name, value, checked, type } = event.target;
-    const val = type === "checkbox" ? checked : value;
 
-    this.setState({
-      [name]: val,
-    });
+  setRef = (node) => {
+    this.formRef = node;
   };
-  handelSubmit = () => {
-    this.props.onSubmit(this.state);
-  };
+
   render() {
     return (
-      <form className="login-form" onSubmit={this.handelSubmit}>
+      <form ref={this.setRef} className="login-form" onSubmit={this.handleSubmit}>
         <h1 className="form-title">Profile</h1>
         <div className="form-control">
           <label className="form-label" htmlFor="name">
             Name
           </label>
-          <input
-            className="form-input"
-            value={this.state.name}
-            type="text"
-            id="name"
-            name="name"
-            onChange={this.handeleChange}
-          />
+          <input className="form-input" type="text" id="name" name="name" />
         </div>
         <div className="form-control">
           <label className="form-label" htmlFor="student">
@@ -41,39 +29,27 @@ class UserForm extends Component {
           </label>
           <input
             className="form-input"
-            value={this.state.student}
             type="checkbox"
             id="student"
             name="student"
-            onChange={this.handeleChange}
           />
         </div>
         <div className="form-control">
           <label className="form-label" id="occupation" htmlFor="occupation">
             Occupation
           </label>
-          <select
-            name="occupation"
-            className="form-input"
-            value={this.state.occupation}
-            onChange={this.handeleChange}
-          >
+          <select name="occupation" className="form-input">
             <option value="london">London</option>
             <option value="new-york">New York</option>
-            <option value="sidney">Sidney</option>
-            <option value="berlin">Berlin</option>
+            <option value="coconut">Sidney</option>
+            <option value="mango">Berlin</option>
           </select>
         </div>
         <div className="form-control">
           <label className="form-label" id="about" htmlFor="about">
             About
           </label>
-          <textarea
-            name="about"
-            onChange={this.handeleChange}
-            value={this.state.about}
-            className="form-input"
-          />
+          <textarea name="about" className="form-input" />
         </div>
         <button className="submit-button" type="submit">
           Submit
