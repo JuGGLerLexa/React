1201a3b32e5542f24cc259ea4f17c16f0bf1461e
Олексій <71986453+JuGGLerLexa@users.noreply.github.com>
2022-07-25 16:09:36 +0300

    Калькулятор температуры воды

---
+++ lesson10/home1/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Calculator from './Calculator';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Calculator />, rootElement);
